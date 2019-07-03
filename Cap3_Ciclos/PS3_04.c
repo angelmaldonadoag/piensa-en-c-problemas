a4f9e5b3263329e645e17e33c442cf9324a13469
Angel Maldonado Aguilar <angelmaldonadoag@gmail.com>
2019-07-02 20:19:54 -0500

    Agregados los primeros 4 problemas del Cap 3

---
+++ Cap3_Ciclos/PS3_04.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**	Clasificador de números enteros.
+*	Programa que, al recibir como datos N números enteros, determina cuántos de
+*	ellos son positivos, negativos o nulos.
+*	Datos: N y NUM. */
+
+int main(int argc, char* argv[])
+{
+	int n, num;
+	int positivos = 0, negativos = 0, nulos = 0;
+
+	printf("Ingrese la cantidad de numeros a clasificar: ");
+	scanf("%d", &n);
+
+	for(int i = 1; i <= n; i++)
+	{
+		printf("Valor del numero %d: ", i);
+		scanf("%d", &num);
+
+		if(num > 0)
+			positivos++;
+		else if(num < 0)
+			negativos++;
+		else
+			nulos++;
+	}
+
+	printf("\nCantidad de numeros positivos: %d\n", positivos);
+	printf("Cantidad de numeros negativos: %d\n", negativos);
+	printf("Cantidad de numeros nulos: %d\n", nulos);
+
+	return 0;
+}
