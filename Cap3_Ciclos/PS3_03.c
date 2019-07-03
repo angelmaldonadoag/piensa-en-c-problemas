6db5e956eef9160b401e87ba9c75acc5de322165
Angel Maldonado Aguilar <angelmaldonadoag@gmail.com>
2019-07-02 20:19:54 -0500

    Agregados los primeros 3 problemas del Cap 3

---
+++ Cap3_Ciclos/PS3_03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**	Serie.
+*	Programa que, al recibir como dato un número entero N, calcula el resultado
+*	de la siguiente serie:
+*	1/1 / 1/2 * 1/3 / 1/4 * ...(/*) 1/N
+*	Dato: N. */
+
+int main(int argc, char* argv[])
+{
+	int n;
+	float resultado = 1.0;
+
+	printf("Ingrese el valor de N: ");
+	scanf("%d", &n);
+
+	for(int i = 1; i <= n; i++)
+	{
+		if( i % 2 == 1 )
+			resultado *= (float)1/i;
+		else if( i % 2 == 0 )
+			resultado /= (float)1/i;
+	}
+
+	printf("\nResultado: %.2f\n", resultado);
+
+	return 0;
+}
