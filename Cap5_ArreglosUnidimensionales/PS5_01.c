50134a873d31b027c9d17e7908ac23b2e3640f93
Angel Maldonado Aguilar <angelmaldonadoag@gmail.com>
2019-07-11 23:22:09 -0500

    Problemas PS5.1 y PS5.2 agregados

---
+++ Cap5_ArreglosUnidimensionales/PS5_01.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**	Clasificación de numeros.
+*	Programa que, al dar como dato un arreglo unidimensional de números enteros
+*	determina cuántos de ellos son positivos, cuántos negativos y cuántos nulos.
+*	Dato: VEC[N] (1 <= N <= 100). */
+
+enum { MAX = 100 };
+
+void leerVec(int*, int);
+void clasificador(int, int*, int* , int*);
+
+int main(int argc, char* argv[])
+{
+	int vec[MAX] = {0}, pos = 0, neg = 0, nul = 0;
+	int n;
+
+	do
+	{
+		printf("Ingrese el tamano del arreglo: ");
+		scanf("%d", &n);
+	} while(n < 1 || n > MAX);
+
+	leerVec(&*vec, n); // Dirección de la indirección == apuntador del vector.
+
+	for(int i = 0; i < n; i++)
+	{
+		clasificador(vec[i], &pos, &neg, &nul);
+	}
+
+	printf("\nPositivos: %d \t Negativos: %d \t Nulos: %d\n", pos, neg, nul);
+
+	return 0;
+}
+
+/* 	La función leerVec se utiliza para leer un arreglo unidimensional. */
+void leerVec(int vector[], int tam)
+{
+	printf("\n");
+
+	for(int i = 0; i < tam; i++)
+	{
+		printf("Ingrese el elemento %d: ", i+1);
+		scanf("%d", &vector[i]);
+	}
+}
+
+/*	La función clasificador determina que tipo de número es el pasado. */
+void clasificador(int num, int *pos, int *neg, int *nul)
+{
+	if(num > 0)
+		*pos += 1;
+	else if(num < 0)
+		*neg += 1;
+	else
+		*nul += 1;
+}
